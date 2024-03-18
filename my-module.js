a19a8acc38f6cddbe21b80d38e36cf40b2d67256
Burak Esit <esitburak@hotmail.com>
2024-03-18 20:27:45 +0000

    First Commit

---
+++ my-module.js
@@ -0,0 +1,32 @@
+ const hello = (name) => {
+    console.log("Hello " + name);
+    console.log(`hello ${name}`)
+};
+
+ const topla = (a, b) => a + b;
+ const cikar = (a, b) => a - b;
+
+ const text = 'Text';
+ const user ={
+    name:'Mehmet',
+    surname:'Seven'
+ }
+ const users = [{
+name:'Ali',
+surname:'Veli'
+ },
+{name:'Kemal',
+surname:'Sunal'}]
+
+
+export {
+    hello,
+    topla,
+    cikar,
+    user,
+    users
+};
+
+
+export default text;
+
