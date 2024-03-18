8777bdbde7961863bbf613b1e52f99b192008ff8
Burak Esit <esitburak@hotmail.com>
2024-03-18 22:50:40 +0000

    Seconda commit

--- my-module.js
+++ my-module.js
@@ -1,32 +1,4 @@
- const hello = (name) => {
-    console.log("Hello " + name);
-    console.log(`hello ${name}`)
-};
+import { getData} from "./app.js";
 
- const topla = (a, b) => a + b;
- const cikar = (a, b) => a - b;
-
- const text = 'Text';
- const user ={
-    name:'Mehmet',
-    surname:'Seven'
- }
- const users = [{
-name:'Ali',
-surname:'Veli'
- },
-{name:'Kemal',
-surname:'Sunal'}]
-
-
-export {
-    hello,
-    topla,
-    cikar,
-    user,
-    users
-};
-
-
-export default text;
+const data = await getData(1);
 
